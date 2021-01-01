2c252b06d4c3aaba7eae6a4056b5c3b39a90fccd
KonuTech <borowiec.k@gmail.com>
2021-01-01 20:55:24 +0100

    KonuTech

--- functions.py
+++ functions.py
@@ -28,6 +28,18 @@ from sklearn.cluster import KMeans, DBSCAN
 from sklearn.decomposition import PCA
 from xgboost import XGBClassifier
 
+import keras
+from keras.layers import Dense, SimpleRNN, LSTM, Activation, Dropout
+from keras.models import Sequential
+from keras.utils import to_categorical
+from keras.optimizers import SGD 
+from keras.callbacks import EarlyStopping
+from keras.utils import np_utils
+from keras.layers import LSTM
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import Dropout
+
 
 import os
 import pandas as pd
@@ -359,3 +371,154 @@ def add_deviation_features(dataframe, variables_floats, variables_objects):
     dataframe = pd.concat([dataframe, output], axis=1)
     
     return dataframe
+
+# Deep Learning
+
+def get_n_last_days(df, series_name, n_days):
+    """
+    Extract last n_days of an hourly time series
+    """
+    
+    return df[series_name][-(n_days):]
+
+def plot_n_last_days(df, series_name, n_days):
+    """
+    Plot last n_days of an hourly time series 
+    """
+    plt.figure(figsize = (10,5))   
+    plt.plot(get_n_last_days(df, series_name, n_days), 'k-')
+    plt.title('{0} Air Quality Time Series - {1} days'.format(series_name, n_days))
+    plt.xlabel('Recorded Hour')
+    plt.ylabel('Value')
+    plt.grid(alpha=0.3)
+    
+def get_keras_format_series(series):
+    """
+    Convert a series to a numpy array of shape 
+    [n_samples, time_steps, features]
+    """
+    
+    series = np.array(series)
+    return series.reshape(series.shape[0], series.shape[1], 1)
+
+def get_train_test_data(df, series_name, series_days, input_days, test_days, sample_gap=3):
+    """
+    Utility processing function that splits an hourly time series into 
+    train and test with keras-friendly format, according to user-specified
+    choice of shape.    
+    
+    arguments
+    ---------
+    df (dataframe): dataframe with time series columns
+    series_name (string): column name in df
+    series_days (int): total days to extract
+    input_days (int): length of sequence input to network 
+    test_days (int): length of held-out terminal sequence
+    sample_gap (int): step size between start of train sequences; default 5
+    
+    returns
+    ---------
+    tuple: train_X, test_X_init, train_y, test_y     
+    """
+    
+    forecast_series = get_n_last_days(df, series_name, series_days).values # reducing our forecast series to last n days
+
+    train = forecast_series[:-test_days] # training data is remaining days until amount of test_days
+    test = forecast_series[-test_days:] # test data is the remaining test_days
+
+    train_X, train_y = [], []
+
+    # range 0 through # of train samples - input_days by sample_gap. 
+    # This is to create many samples with corresponding
+    for i in range(0, train.shape[0]-input_days, sample_gap): 
+        train_X.append(train[i:i+input_days]) # each training sample is of length input hours
+        train_y.append(train[i+input_days]) # each y is just the next step after training sample
+
+    train_X = get_keras_format_series(train_X) # format our new training set to keras format
+    train_y = np.array(train_y) # make sure y is an array to work properly with keras
+    
+    # The set that we had held out for testing (must be same length as original train input)
+    test_X_init = test[:input_days] 
+    test_y = test[input_days:] # test_y is remaining values from test set
+    
+    return train_X, test_X_init, train_y, test_y
+
+def fit_SimpleRNN(train_X, train_y, cell_units, epochs):
+    """
+    Fit Simple RNN to data train_X, train_y 
+    
+    arguments
+    ---------
+    train_X (array): input sequence samples for training 
+    train_y (list): next step in sequence targets
+    cell_units (int): number of hidden units for RNN cells  
+    epochs (int): number of training epochs   
+    """
+
+    # initialize model
+    model = Sequential() 
+    
+    # construct an RNN layer with specified number of hidden units
+    # per cell and desired sequence input format 
+    model.add(SimpleRNN(cell_units, input_shape=(train_X.shape[1],1)))
+    
+    # add an output layer to make final predictions 
+    model.add(Dense(1))
+    
+    # define the loss function / optimization strategy, and fit
+    # the model with the desired number of passes over the data (epochs) 
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(train_X, train_y, epochs=epochs, verbose=1)
+    
+    return model
+
+def predict(X_init, n_steps, model):
+    """
+    Given an input series matching the model's expected format,
+    generates model's predictions for next n_steps in the series      
+    """
+    
+    X_init = X_init.copy().reshape(1,-1,1)
+    preds = []
+    
+    # iteratively take current input sequence, generate next step pred,
+    # and shift input sequence forward by a step (to end with latest pred).
+    # collect preds as we go.
+    for _ in range(n_steps):
+        pred = model.predict(X_init)
+        preds.append(pred)
+        X_init[:,:-1,:] = X_init[:,1:,:] # replace first 11 values with 2nd through 12th
+        X_init[:,-1,:] = pred # replace 12th value with prediction
+    
+    preds = np.array(preds).reshape(-1,1)
+    
+    return preds
+
+def predict_and_plot(X_init, y, model, title):
+    """
+    Given an input series matching the model's expected format,
+    generates model's predictions for next n_steps in the series,
+    and plots these predictions against the ground truth for those steps 
+    
+    arguments
+    ---------
+    X_init (array): initial sequence, must match model's input shape
+    y (array): true sequence values to predict, follow X_init
+    model (keras.models.Sequential): trained neural network
+    title (string): plot title   
+    """
+    
+    y_preds = predict(X_init, n_steps=len(y), model=model) # predict through length of y
+    # Below ranges are to set x-axes
+    start_range = range(1, test_X_init.shape[0]+1) #starting at one through to length of test_X_init to plot X_init
+    predict_range = range(test_X_init.shape[0], test_days)  #predict range is going to be from end of X_init to length of test_days
+    
+    #using our ranges we plot X_init
+    plt.plot(start_range, test_X_init)
+    #and test and actual preds
+    plt.plot(predict_range, test_y, color='orange')
+    plt.plot(predict_range, y_preds, color='teal', linestyle='--')
+    
+    plt.title(title)
+    plt.legend(['Initial Series','Target Series','Predictions'])
+    
